c8d00fab62d73bae0ae3fccce2c4b5e53dcdb226
ndylanl <dylanlienardi@gmail.com>
2024-05-03 04:45:51 +0700

    3.2 - Ongoing - Section 1 Step 7 - Crashes adding Environment in Profile Summary

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Nicholas Dylan Lienardi on 03/05/24.
+//
+
+import Foundation
+
+struct Profile{
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "DaC00KIE")
+    
+    enum Season: String, CaseIterable, Identifiable{
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
